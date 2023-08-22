08fbf64e739e5b257c25996cb6383dc1fa8c5021
Raymond Prasetio <raymondprasetio22@gmail.com>
2023-08-22 22:18:16 +0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Raymond Prasetio on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
